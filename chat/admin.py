9979b185781d8809801697263dab2bdc01ab21aa
Bernhard Sigl <b-sigl@live.de>
2024-04-22 19:55:04 +0200

    authenticate

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,12 @@
+from .models import Chat, Message, Login
 from django.contrib import admin
 
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('chat', 'text', 'created_at', 'author', 'receiver')
+    list_display = ('text', 'created_at', 'author', 'receiver')
+    search_fields = ('text',)
+
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
+admin.site.register(Login)
--- chat/models.py
+++ chat/models.py
@@ -1,25 +1,16 @@
 from django.db import models
+from datetime import date
+from django.conf import settings
 
-from django.utils import timezone
+class Login(models.Model):
+  text = models.CharField(max_length=500)
 
+class Chat(models.Model):
+  created_at = models.DateField(default=date.today)
 
-
-class Book(models.Model):
-
-  title = models.CharField(max_length=200)
-
-  author = models.CharField(max_length=100)
-
-  publish_date = models.DateField(default=timezone.now)
-
-  isbn = models.CharField(max_length=13, unique=True)
-
-  page_count = models.IntegerField()
-
-  description = models.TextField(blank=True)
-
-
-
-  def __str__(self):
-
-    return self.title
+class Message(models.Model):
+  text = models.CharField(max_length=500)
+  created_at = models.DateField(default=date.today)
+  chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat_message_set', default=None, blank=True, null=True)
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
+  receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
